d034178e4fdd4206011f8accf639cead87419777
Andre Felipe <afelipes@gmail.com>
2022-11-21 15:47:26 -0300

    feat:challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int) or not isinstance(target_time, int):
+        return None
+    students = 0
+
+    for init, end in permanence_period:
+        if not isinstance(init, int) or not isinstance(end, int):
+            return None
+        elif init <= target_time <= end:
+            students += 1
+    return students
+    # raise NotImplementedError
